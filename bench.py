f0736493093f4b814fbc0898ad852bac67634696
ztx <1952309136@qq.com>
2025-09-04 23:01:54 +0800

    config env and run example and bench

--- bench.py
+++ bench.py
@@ -11,7 +11,7 @@ def main():
     max_input_len = 1024
     max_ouput_len = 1024
 
-    path = os.path.expanduser("~/huggingface/Qwen3-0.6B/")
+    path = os.path.expanduser("/root/workspace/nano-vllm-learn/huggingface/Qwen3-0.6B")
     llm = LLM(path, enforce_eager=False, max_model_len=4096)
 
     prompt_token_ids = [[randint(0, 10000) for _ in range(randint(100, max_input_len))] for _ in range(num_seqs)]
